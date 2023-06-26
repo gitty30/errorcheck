ee3aad5cc123cec178b090fea5b6e5ce7a2381d8
Archie180 <archie0108.be20@chitkara.edu.in>
2023-06-26 17:09:25 +0530

    first

---
+++ src/routes/+page.svelte
@@ -0,0 +1,20 @@
+
+<script>
+ 
+
+const throwError=()=>{
+try {
+  abc();
+} catch (error) {
+  console.error("Error: " + error);
+  throw new Error(error)
+}
+}
+
+</script>
+
+<button on:click={throwError}>Throw Error</button>
+
+
+
+
